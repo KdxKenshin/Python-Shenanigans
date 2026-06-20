de6a839d5e923fc1d9fdde06b52d1d82f0cfc830
Tapiwa Machekera <kevinwariita10@gmail.com>
2026-06-20 13:59:01 +0200

    Number Guessing Game
    Simple number guessing game: the player enters an upper bound, then tries to guess a randomly chosen number within that range.
    The program gives feedback whether each guess is too high or too low, and counts the number of attempts.

---
+++ Number Guessing Game/number_guesser.py
@@ -0,0 +1,36 @@
+# Simple number guessing game: the player enters an upper bound, then tries to guess a randomly chosen number within that range.
+# The program gives feedback whether each guess is too high or too low, and counts the number of attempts.
+import random
+
+top_of_range = input("Type a number: ")
+## Check if the input is a digit and convert it to an integer 
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    if top_of_range <= 0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+else:
+    print("Please type a number next time.")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+    
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+            print("You were above the number!")
+    else:
+            print("You were below the number!")
+
+print("You got it in", guesses, "guesses")        
